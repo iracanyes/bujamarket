2f779f97a42cc7298403c1f2dc19e943fe1fa594
Iracanyes <iracanyes@isl-edu.be>
2020-03-03 06:34:08 +0100

    Windows update

--- api/src/Repository/UserTempRepository.php
+++ api/src/Repository/UserTempRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\UserTemp;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method UserTemp|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class UserTempRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, UserTemp::class);
     }
